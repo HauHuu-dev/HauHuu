59b2f4b8ab3d66dbbd168deefff6e073ee4422b8
nguyenhauhuu <huu_dpm225425@student.agu.edu.vn>
2025-09-26 19:35:38 +0700

    chỉnh Astronaut fix null warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import About from "./sections/About";
 import Contact from "./sections/Contact";
 import Hero from "./sections/Hero";
-import Navbar from "./sections/Navbar";
 
 function App() {
     return (
